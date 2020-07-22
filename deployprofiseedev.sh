1835e798a3d0439724aca5a85db79ffcad1c429f
Profisee <63325482+profiseegroup@users.noreply.github.com>
2020-07-22 16:18:39 -0400

    Update deployprofiseedev.sh

--- deployprofiseedev.sh
+++ deployprofiseedev.sh
@@ -63,6 +63,8 @@ azureAppReplyUrl="${EXTERNALDNSURL}/profisee/auth/signin-microsoft"
 if [ "$UPDATEAAD" = "Yes" ]; then
 	azureClientName="${RESOURCEGROUPNAME}_${CLUSTERNAME}";
 	CLIENTID=$(az ad app create --display-name $azureClientName --reply-urls $azureAppReplyUrl --query 'appId');
+	#clean client id - remove quotes
+	CLIENTID=$(echo "$CLIENTID" | tr -d '"')
 fi
 
 #get storage account pw
@@ -103,7 +105,23 @@ sed -i -e 's/$ACRREPOLABEL/'"$ACRREPOLABEL"'/g' Settings.yaml
 
 helm repo add profisee https://profisee.github.io/kubernetes
 helm uninstall profiseeplatform2020r1
-helm install profiseeplatform2020r1 profisee/profisee-platform --values Settings.yaml
+helm install profiseeplatform2020r1 profisee/profisee-platform --values Settings.yaml 
+--set sqlServer.name=$SQLNAME 
+--set sqlServer.databaseName=$SQLDBNAME 
+--set sqlServer.userName=$SQLUSERNAME 
+--set sqlServer.password=$SQLUSERPASSWORD 
+--set profiseeRunTime.fileRepository.userName=$FILEREPOUSERNAME 
+--set profiseeRunTime.fileRepository.password=$FILEREPOPASSWORD 
+--set profiseeRunTime.fileRepository.location=$FILEREPOURL 
+--set profiseeRunTime.oidc.authority=$OIDCURL 
+--set profiseeRunTime.oidc.clientId=$CLIENTID 
+--set profiseeRunTime.oidc.clientSecret=$OIDCCLIENTSECRET 
+--set profiseeRunTime.adminAccount=$ADMINACCOUNTNAME 
+--set profiseeRunTime.externalDnsUrl=$EXTERNALDNSURL 
+--set profiseeRunTime.externalDnsName=$EXTERNALDNSNAME 
+--set licenseFileData=$LICENSEDATA 
+--set image.repository=$ACRREPONAME 
+--set image.tag=$ACRREPOLABEL
 
 result="{\"Result\":[\
 {\"IP\":\"$nginxip\"},\
