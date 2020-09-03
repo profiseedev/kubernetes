f3c31733421d2b6e7c05aba1998a365a21b33db4
Profisee <63325482+profiseegroup@users.noreply.github.com>
2020-09-03 17:28:00 -0400

    Update deployprofisee.sh

--- deployprofisee.sh
+++ deployprofisee.sh
@@ -13,12 +13,17 @@ chmod 700 get_helm.sh;
 ./get_helm.sh;
 
 #install nginx
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/;
+#helm repo add stable https://kubernetes-charts.storage.googleapis.com/;
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo update
+
 #get profisee nginx settings
 curl -fsSL -o nginxSettings.yaml https://raw.githubusercontent.com/profiseegroup/kubernetes/master/nginxSettings.yaml;
 helm uninstall nginx
 #helm install nginx stable/nginx-ingress --values nginxSettings.yaml --set controller.service.loadBalancerIP=$publicInIP;
-helm install nginx nginx-stable/nginx-ingress --set controller.service.loadBalancerIP=$publicInIP;
+helm install nginx ingress-nginx/ingress-nginx --set controller.service.loadBalancerIP=$publicInIP;
 
 
 #wait for the ip to be available.  usually a few seconds
