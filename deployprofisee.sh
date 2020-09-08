ac81dc796709466ce81dd10ac43f149b3b1583fe
Profisee <63325482+profiseegroup@users.noreply.github.com>
2020-09-08 17:14:49 -0400

    Update deployprofisee.sh

--- deployprofisee.sh
+++ deployprofisee.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#exec 3>&1 4>&2
-#trap 'exec 2>&4 1>&3' 0 1 2 3
-#exec 1>log.out 2>&1
 #install the aks cli since this script runs in az 2.0.80 and the az aks was not added until 2.5
 az aks install-cli;
 #get the aks creds, this allows us to use kubectl commands if needed
@@ -14,17 +11,10 @@ chmod 700 get_helm.sh;
 
 #install nginx
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/;
-
-#helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-#helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo update
-
 #get profisee nginx settings
 curl -fsSL -o nginxSettings.yaml https://raw.githubusercontent.com/profiseegroup/kubernetes/master/nginxSettings.yaml;
 helm uninstall nginx
 helm install nginx stable/nginx-ingress --values nginxSettings.yaml --set controller.service.loadBalancerIP=$publicInIP;
-#helm install nginx ingress-nginx/ingress-nginx --values nginxSettings.yaml --set controller.service.loadBalancerIP=$publicInIP;
-
 
 #wait for the ip to be available.  usually a few seconds
 sleep 30;
@@ -40,7 +30,7 @@ if [ "$CONFIGUREHTTPS" = "Yes" ]; then
 else    
     echo '    NA' > tls.cert;
 fi
-#rm a.cert
+rm a.cert
 
 #key
 if [ "$CONFIGUREHTTPS" = "Yes" ]; then
@@ -108,8 +98,6 @@ sed -i -e 's/$SQLUSERPASSWORD/'"$SQLUSERPASSWORD"'/g' Settings.yaml
 sed -i -e 's/$FILEREPOUSERNAME/'"$FILEREPOUSERNAME"'/g' Settings.yaml
 sed -i -e 's~$FILEREPOPASSWORD~'"$FILEREPOPASSWORD"'~g' Settings.yaml
 sed -i -e 's/$FILEREPOURL/'"$FILEREPOURL"'/g' Settings.yaml
-sed -i -e 's/$FILESHARENAME/'"$STORAGEACCOUNTFILESHARENAME"'/g' Settings.yaml
-sed -i -e 's/$FILEACCOUNTNAME/'"$STORAGEACCOUNTNAME"'/g' Settings.yaml
 sed -i -e 's~$OIDCURL~'"$OIDCURL"'~g' Settings.yaml
 sed -i -e 's/$CLIENTID/'"$CLIENTID"'/g' Settings.yaml
 sed -i -e 's/$OIDCCLIENTSECRET/'"$OIDCCLIENTSECRET"'/g' Settings.yaml
