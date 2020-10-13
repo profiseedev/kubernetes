c5aa2c36d519e9f8364f3c58f883dc55270a57dd
chuckgmthompson <chuckgmthompson@hotmail.com>
2020-10-12 23:59:30 -0400

    Update deployprofisee.sh

--- Azure-ARM/deployprofisee.sh
+++ Azure-ARM/deployprofisee.sh
@@ -48,8 +48,7 @@ echo $"EXTERNALDNSNAME is $EXTERNALDNSNAME";
 echo $"DNSHOSTNAME is $DNSHOSTNAME";
 
 #If acr info is passed in (via legacy script) use it, otherwise pull it from license
-if [-z "${ACRUSER}"]; then
-#if ["$ACRUSER"=""]; then
+if [-z "${ACRUSER}"] || ["$ACRUSER"=""]; then
 	echo $"ACRUSER is empty, pulling from license"
 	ACRUSER=$(<ACRUserName.txt)
 	ACRUSERPASSWORD=$(<ACRUserPassword.txt)
