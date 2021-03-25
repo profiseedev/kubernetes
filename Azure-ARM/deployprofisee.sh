059b01448cb2c75a2c9bbaed20ec6a81381b8d73
chuckgmthompson <chuck.thompson@profisee.com>
2021-03-25 16:28:42 -0400

    Update deployprofisee.sh

--- Azure-ARM/deployprofisee.sh
+++ Azure-ARM/deployprofisee.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
+exec 1>log_$(date +%Y-%m-%d_%H-%M-%S).out 2>&1
 
 echo $"Profisee deploymented started $(date +"%Y-%m-%d %T")";
 
