b2e4891edb64af9f196e5f1d4bfaced542ddc2a7
chuckgmthompson <chuck.thompson@profisee.com>
2021-07-26 15:22:55 -0400

    Update prereqcheck.sh

--- Azure-ARM/prereqcheck.sh
+++ Azure-ARM/prereqcheck.sh
@@ -54,8 +54,9 @@ if [ -z "$subscriptionContributor" ]; then
 	#not subscription level, check resource group level
 	rgContributor=$(az role assignment list --all --assignee $currentIdentityId --output json --include-inherited --query "[?roleDefinitionName=='Contributor' && scope=='/subscriptions/$SUBSCRIPTIONID/resourceGroups/$RESOURCEGROUPNAME'].roleDefinitionName" --output tsv)
 	if [ -z "$rgContributor" ]; then
-		echo "Managed identity is not contributor to resource group.  Exiting with error"
-		exit;# 1
+		err="Managed identity is not contributor to resource group.  Exiting with error."
+		echo $err
+		set_resultAndReturn;
 	else
 		echo "Managed identity is contributor to resource group."
 	fi
@@ -65,14 +66,9 @@ if [ -z "$subscriptionContributor" ]; then
 		echo "Checking contributor for DNS resource group"
 		dnsrgContributor=$(az role assignment list --all --assignee $currentIdentityId --output json --include-inherited --query "[?roleDefinitionName=='Contributor' && scope=='/subscriptions/$SUBSCRIPTIONID/resourceGroups/$DOMAINNAMERESOURCEGROUP'].roleDefinitionName" --output tsv)
 		if [ -z "$dnsrgContributor" ]; then
-			err="Managed identity is not contributor to DNS resource group.  Exiting with error"
+			err="Managed identity is not contributor to DNS resource group.  Exiting with error."
 			echo $err
-			result="{\"Result\":[\
-			{\"SUCCESS\":\"$success\"},
-			{\"ERROR\":\"$err\"}\
-			]}"
-			echo $result > $AZ_SCRIPTS_OUTPUT_PATH
-			exit;# 1
+			set_resultAndReturn;
 		else
 			echo "Managed identity is contributor to DNS resource group."
 		fi
@@ -84,8 +80,9 @@ if [ -z "$subscriptionContributor" ]; then
 		KEYVAULT=$(echo $KEYVAULT | xargs)
 		kvContributor=$(az role assignment list --all --assignee $currentIdentityId --output json --include-inherited --query "[?roleDefinitionName=='Contributor' && scope=='$KEYVAULT'].roleDefinitionName" --output tsv)
 		if [ -z "$kvContributor" ]; then
-			echo "Managed identity is not contributor to KeyVault.  Exiting with error"
-			exit 1
+			err="Managed identity is not contributor to KeyVault.  Exiting with error."
+			echo $err
+			set_resultAndReturn;
 		else
 			echo "Managed identity is contributor to KeyVault."
 		fi
@@ -100,8 +97,9 @@ if [ "$USEKEYVAULT" = "Yes" ]; then
 	echo "Checking Managed Identity Contributor"
 	subscriptionMIContributor=$(az role assignment list --all --assignee $currentIdentityId --output json --include-inherited --query "[?roleDefinitionName=='Managed Identity Contributor' && scope=='/subscriptions/$SUBSCRIPTIONID'].roleDefinitionName" --output tsv)
 	if [ -z "$subscriptionMIContributor" ]; then
-		echo "Managed identity is not Managed Identity Contributor.  Exiting with error"
-		exit 1
+		err="Managed identity is not Managed Identity Contributor.  Exiting with error."
+		echo $err
+		set_resultAndReturn;
 	else
 		echo "Managed identity is Managed Identity Contributor."
 	fi
@@ -113,8 +111,9 @@ if [ "$UPDATEAAD" = "Yes" ]; then
 	appDevRoleId=$(az rest --method get --url https://graph.microsoft.com/v1.0/directoryRoles/ | jq -r '.value[] | select(.displayName | contains("Application Developer")).id')
 	minameinrole=$(az rest --method GET --uri "https://graph.microsoft.com/beta/directoryRoles/$appDevRoleId/members" | jq -r '.value[] | select(.displayName | contains("'"$miname"'")).displayName')
 	if [ -z "$minameinrole" ]; then
-		echo "Managed identity is not in application developer role.  exiting with error"
-		exit 1
+		err="Managed identity is not in application developer role.  exiting with error."
+		echo $err
+		set_resultAndReturn;
 	else
 		echo "Managed identity is in application developer role."
 	fi
@@ -129,6 +128,12 @@ result="{\"Result\":[\
 {\"SUCCESS\":\"$success\"},
 ]}"
 echo $result > $AZ_SCRIPTS_OUTPUT_PATH
-echo $result
 
-echo $result > $AZ_SCRIPTS_OUTPUT_PATH
+set_resultAndReturn () {
+	result="{\"Result\":[\
+	{\"SUCCESS\":\"$success\"},
+	{\"ERROR\":\"$err\"}\
+	]}"
+	echo $result > $AZ_SCRIPTS_OUTPUT_PATH
+	exit 1
+}
