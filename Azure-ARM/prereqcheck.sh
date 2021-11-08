9103408e06e3f06ca27ba17e8de9b931666e7737
jamesridgwayprofisee <76177269+jamesridgwayprofisee@users.noreply.github.com>
2021-11-08 09:50:13 -0500

    Update prereqcheck.sh
    Added missing line for data curator role check.

--- Azure-ARM/prereqcheck.sh
+++ Azure-ARM/prereqcheck.sh
@@ -111,6 +111,7 @@ fi
 if [ "$USEPURVIEW" = "Yes" ]; then
 
 	# Data Curator 
+	principalObjId=$(az ad sp list --filter "AppId eq '$PURVIEWCLIENTID'" | jq -r .[0].objectId)
 	collectionName=$(az rest --method get --url ${PURVIEWURL}collections?api-version=2019-11-01-preview --resource 73c2949e-da2d-457a-9607-fcc665198967 | jq -r .value[0].name)
 	policyId=$(az rest --method get --url  ${PURVIEWURL}policyStore/metadataPolicies?collectionName=$collectionName --resource 73c2949e-da2d-457a-9607-fcc665198967 | jq -r .values[0].id)
 	curatorId=$(az rest --method get --url ${PURVIEWURL}policystore/metadataPolicies/$policyId?api-version=2021-07-01 --resource 73c2949e-da2d-457a-9607-fcc665198967 | jq -r '.properties.attributeRules[] | select(.id | startswith("purviewmetadatarole_builtin_data-curator:")) | .dnfCondition[][] | select(.attributeName == "principal.microsoft.id") | .attributeValueIncludedIn[] | select(contains("'$principalObjId'"))');
