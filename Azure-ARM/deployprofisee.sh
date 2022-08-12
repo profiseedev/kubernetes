9654bd463fe755a9e351bfdf5a29fcf4c5196ed6
Yonko Ganchev <yonko.ganchev@profisee.com>
2022-08-12 17:04:57 -0400

    Updated chart to 0.1.20
    Updated deployprofisee.sh with various updates, Readme, csi api version from v1alpha1 to v1

--- Azure-ARM/deployprofisee.sh
+++ Azure-ARM/deployprofisee.sh
@@ -105,8 +105,15 @@ install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 echo $"Installation of kubectl finished.";
 
 #Create profisee namespace in AKS cluster.
-echo $"Creation of profisee namespace in cluster started.";
-kubectl create namespace profisee
+echo $"Creation of profisee namespace in cluster started. If present, we skip creation and use it.";
+
+#If namespace exists, skip creating it.
+namespacepresent=$(kubectl get namespace -o jsonpath='{.items[?(@.metadata.name=="profisee")].metadata.name}')
+if [ "$namespacepresent" = "profisee" ]; then
+	echo $"Namespace is already created, continuing."
+else
+	kubectl create namespace profisee
+fi
 echo $"Creation of profisee namespace in cluster finished.";
 
 #Download Settings.yaml file from Profisee repo.
@@ -114,20 +121,37 @@ curl -fsSL -o Settings.yaml "$REPOURL/Azure-ARM/Settings.yaml";
 
 #Installation of Key Vault Container Storage Interface (CSI) driver started.
 if [ "$USEKEYVAULT" = "Yes" ]; then
-	echo $"Installation of Key Vault Container Storage Interface (CSI) driver started."
+	echo $"Installation of Key Vault Container Storage Interface (CSI) driver started. If present, we uninstall and reinstall it."
 	#Install the Secrets Store CSI driver and the Azure Key Vault provider for the driver
 	helm repo add csi-secrets-store-provider-azure https://azure.github.io/secrets-store-csi-driver-provider-azure/charts
+	
+	#If Key Vault CSI driver is present, uninstall it.
+        kvcsipresent=$(helm list -n profisee -f csi-secrets-store-provider-azure -o table --short)
+        if [ "$kvcsipresent" = "csi-secrets-store-provider-azure" ]; then
+	        helm uninstall -n profisee csi-secrets-store-provider-azure;
+	        echo $"Will sleep for 30 seconds to allow clean uninstall of Key Vault CSI driver."
+	        sleep 30;
+        fi
 	
 	#https://github.com/Azure/secrets-store-csi-driver-provider-azure/releases/tag/0.0.16
 	#The behavior changed so now you have to enable the secrets-store-csi-driver.syncSecret.enabled=true
 	#We are not but if this is to run on a windows node, then you use this --set windows.enabled=true --set secrets-store-csi-driver.windows.enabled=true
-	helm install --namespace profisee csi-secrets-store-provider-azure csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --set secrets-store-csi-driver.syncSecret.enabled=true
+	helm install -n profisee csi-secrets-store-provider-azure csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --set secrets-store-csi-driver.syncSecret.enabled=true
 	echo $"Installation of Key Vault Container Storage Interface (CSI) driver finished."
 	
+	
 	#Install AAD pod identity into AKS.
-	echo $"Installation of Key Vault Azure Active Directory Pod Identity driver started."
+	echo $"Installation of Key Vault Azure Active Directory Pod Identity driver started. If present, we uninstall and reinstall it."
+	#If AAD Pod Identity is present, uninstall it.
+        aadpodpresent=$(helm list -n profisee -f pod-identity -o table --short)
+        if [ "$aadpodpresent" = "pod-identity" ]; then
+	        helm uninstall -n profisee pod-identity;
+	        echo $"Will sleep for 30 seconds to allow clean uninstall of AAD Pod Identity."
+	        sleep 30;
+        fi
+	
 	helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts
-	helm install --namespace profisee pod-identity aad-pod-identity/aad-pod-identity
+	helm install -n profisee pod-identity aad-pod-identity/aad-pod-identity
 	echo $"Installation of Key Vault Azure Active Directory Pod Identity driver finished."
 
 	#Assign AAD roles to the AKS AgentPool Managed Identity. The Pod identity communicates with the AgentPool MI, which in turn communicates with the Key Vault specific Managed Identity.
@@ -196,39 +220,44 @@ fi
 
 #Installation of nginx
 echo $"Installation of nginx ingress started.";
-
+echo $"Adding ingress-nginx repo."
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 
 #Get profisee nginx settings
+echo $"Acquiring nginxSettings.yaml file from Profisee repo."
 curl -fsSL -o nginxSettings.yaml "$REPOURL/Azure-ARM/nginxSettings.yaml";
 
 #If nginx is present, uninstall it.
+echo "If nginx is installed, we'll uninstall it first."
 nginxpresent=$(helm list -n profisee -f nginx -o table --short)
 if [ "$nginxpresent" = "nginx" ]; then
 	helm uninstall -n profisee nginx;
+	echo $"Will sleep for a minute to allow clean uninstall of nginx."
+	sleep 60;
 fi
 
 #Install nginx either with or without Let's Encrypt
 echo $"Installation of nginx started.";
 if [ "$USELETSENCRYPT" = "Yes" ]; then
 	echo $"Install nginx ready to integrate with Let's Encrypt's automatic certificate provisioning and renewal, and set the DNS FQDN to the load balancer's ingress public IP address."
-	helm install --namespace profisee nginx ingress-nginx/ingress-nginx --values nginxSettings.yaml --set controller.service.loadBalancerIP=$nginxip --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"=$DNSHOSTNAME;
+	helm install -n profisee nginx ingress-nginx/ingress-nginx --values nginxSettings.yaml --set controller.service.loadBalancerIP=$nginxip --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"=$DNSHOSTNAME;
 else
 	echo $"Install nginx without integration with Let's Encrypt's automatic certificate provisioning and renewal, also do not set the DNS FQDN to the load balancer's ingress public IP address."
-	helm install --namespace profisee nginx ingress-nginx/ingress-nginx --values nginxSettings.yaml --set controller.service.loadBalancerIP=$nginxip
+	helm install -n profisee nginx ingress-nginx/ingress-nginx --values nginxSettings.yaml --set controller.service.loadBalancerIP=$nginxip
 fi
 
 echo $"Installation of nginx finished, sleeping for 30 seconds to wait for the load balancer's public IP to become available.";
 sleep 30;
 
 #Get the load balancer's public IP so it can be used later on.
-nginxip=$(kubectl --namespace profisee get services nginx-ingress-nginx-controller --output="jsonpath={.status.loadBalancer.ingress[0].ip}");
+echo $"Let's see if the the load balancer's IP address is available."
+nginxip=$(kubectl -n profisee get services nginx-ingress-nginx-controller --output="jsonpath={.status.loadBalancer.ingress[0].ip}");
 #
 if [ -z "$nginxip" ]; then
 	#try again
 	echo $"Nginx is not configured properly because the load balancer's public IP is null, will wait for another minute.";
     sleep 60;
-	nginxip=$(kubectl --namespace profisee get services nginx-ingress-nginx-controller --output="jsonpath={.status.loadBalancer.ingress[0].ip}");
+	nginxip=$(kubectl -n profisee get services nginx-ingress-nginx-controller --output="jsonpath={.status.loadBalancer.ingress[0].ip}");
 	if [ -z "$nginxip" ]; then
     	echo $"Nginx is not configured properly because the load balancer's public IP is null. Exiting with error.";
 		exit 1
@@ -307,18 +336,26 @@ if [ "$UPDATEAAD" = "Yes" ]; then
 	echo "Sleeping for 20 seconds to wait for the app registration to be ready."
 	sleep 20;
 
-	echo "Update of the application registration's permissions, step 1 started."
-	#Add a Graph API permission to "Sign in and read user profile"
-	az ad app permission add --id $CLIENTID --api 00000003-0000-0000-c000-000000000000 --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope
-	echo "Creation of the service principal started."
-	az ad sp create --id $CLIENTID
-	echo "Creation of the service principal finished."
-	echo "Update of the application registration's permissions, step 1 finished."
+	#If Azure Application Registration User.Read permission is present, skip adding it.
+	echo $"Let's check to see if the User.Read permission is granted, skip if has been."
+        appregpermissionspresent=$(az ad app permission list --id $CLIENTID --query "[].resourceAccess[].id" -o tsv)
+        if [ "$appregpermissionspresent" = "e1fe6dd8-ba31-4d61-89e7-88639da4683d" ]; then
+	        echo $"User.Read permissions already present, no need to add it."
+	else
+	
+	        echo "Update of the application registration's permissions, step 1 started."
+	        #Add a Graph API permission to "Sign in and read user profile"
+	        az ad app permission add --id $CLIENTID --api 00000003-0000-0000-c000-000000000000 --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope
+	        echo "Creation of the service principal started."
+	        az ad sp create --id $CLIENTID
+	        echo "Creation of the service principal finished."
+	        echo "Update of the application registration's permissions, step 1 finished."
 
-	echo "Update of the application registration's permissions, step 2 started."
-	az ad app permission grant --id $CLIENTID --api 00000003-0000-0000-c000-000000000000
-	echo "Update of the application registration's permissions, step 2 finished."
-	echo "Update of Azure Active Directory finished.";
+	        echo "Update of the application registration's permissions, step 2 started."
+	        az ad app permission grant --id $CLIENTID --api 00000003-0000-0000-c000-000000000000
+	        echo "Update of the application registration's permissions, step 2 finished."
+	        echo "Update of Azure Active Directory finished.";
+	fi
 fi
 
 #If not supplied, acquire storage account credentials.
@@ -405,7 +442,6 @@ if [ "$USEKEYVAULT" = "Yes" ]; then
 	sed -i -e 's/$AZURESUBSCRIPTIONID/'"$keyVaultSubscriptionId"'/g' Settings.yaml
 	sed -i -e 's/$AZURETENANTID/'"$TENANTID"'/g' Settings.yaml
 
-	$SUBSCRIPTIONID
 else
 	sed -i -e 's/$USEKEYVAULT/'false'/g' Settings.yaml
 fi
@@ -418,8 +454,15 @@ if [ "$USELETSENCRYPT" = "Yes" ]; then
 	helm repo add jetstack https://charts.jetstack.io
 	# Update your local Helm chart repository cache
 	helm repo update
+	#If cert-manager is present, uninstall it.
+        certmgrpresent=$(helm list -n profisee -f cert-manager -o table --short)
+        if [ "$certmgrpresent" = "cert-manager" ]; then
+	        helm uninstall -n profisee cert-manager;
+	        echo $"Will sleep for 20 seconds to allow clean uninstall of cert-manager."
+	        sleep 20;
+        fi
 	# Install the cert-manager Helm chart
-	helm install cert-manager jetstack/cert-manager --namespace profisee --set installCRDs=true --set nodeSelector."kubernetes\.io/os"=linux --set webhook.nodeSelector."kubernetes\.io/os"=linux --set cainjector.nodeSelector."kubernetes\.io/os"=linux --set startupapicheck.nodeSelector."kubernetes\.io/os"=linux
+	helm install cert-manager jetstack/cert-manager -n profisee --set installCRDs=true --set nodeSelector."kubernetes\.io/os"=linux --set webhook.nodeSelector."kubernetes\.io/os"=linux --set cainjector.nodeSelector."kubernetes\.io/os"=linux --set startupapicheck.nodeSelector."kubernetes\.io/os"=linux
 	# Wait for the cert manager to be ready
 	echo $"Let's Encrypt is waiting for certificate manager to be ready, sleeping for 30 seconds.";
 	sleep 30;
@@ -431,8 +474,8 @@ else
 fi
 
 #Adding Settings.yaml as a secret generated only from the initial deployment of Profisee. Future updates, such as license changes via the profisee-license secret, or SQL credentials updates via the profisee-sql-password secret, will NOT be reflected in this secret. Proceed with caution!
-kubectl delete secret profisee-settings --namespace profisee --ignore-not-found
-kubectl create secret generic profisee-settings --namespace profisee --from-file=Settings.yaml
+kubectl delete secret profisee-settings -n profisee --ignore-not-found
+kubectl create secret generic profisee-settings -n profisee --from-file=Settings.yaml
 
 #################################Install Profisee Start #######################################
 echo "Installation of Profisee platform started $(date +"%Y-%m-%d %T")";
@@ -440,18 +483,34 @@ helm repo add profisee $HELMREPOURL
 helm repo update
 
 #If Profisee is present, uninstall it. If not, proceeed to installation.
+echo "If profisee is installed, uninstall it first."
 profiseepresent=$(helm list -n profisee -f profiseeplatform -o table --short)
 if [ "$profiseepresent" = "profiseeplatform" ]; then
 	helm -n profisee uninstall profiseeplatform;
+	echo "Will sleep for 30 seconds to allow clean uninstall."
+	sleep 30;
 fi
 
-helm install --namespace profisee profiseeplatform profisee/profisee-platform --values Settings.yaml
-
-kubectl delete secret profisee-deploymentlog --namespace profisee --ignore-not-found
-kubectl create secret generic profisee-deploymentlog --namespace profisee --from-file=$logfile
+echo "If we are using Key Vault and Profisee was uninstalled, then the profisee-license, profisee-sql-username, profisee-sql-password and profisee-tls-ingress secrets are missing. We need to find and restart the key-vault pod so that we can re-pull and re-mount the secrets."
+#Find and delete the key-vault pod
+if [ "$USEKEYVAULT" = "Yes" ]; then
+	findkvpod=$(kubectl get pods -n profisee -o jsonpath='{.items[?(@.metadata.labels.app=="profisee-keyvault")].metadata.name}')
+	if [ "$findkvpod" = "$findkvpod" ]; then
+	echo $"Profisee Key Vault pod name is $findkvpod, deleting it."
+	kubectl delete pod -n profisee $findkvpod --force --grace-period=0
+	echo "Now let's install Profisee."
+	helm install -n profisee profiseeplatform profisee/profisee-platform --values Settings.yaml
+	fi
+else
+	echo "Now let's install Profisee."
+	helm install -n profisee profiseeplatform profisee/profisee-platform --values Settings.yaml
+fi
+	
+kubectl delete secret profisee-deploymentlog -n profisee --ignore-not-found
+kubectl create secret generic profisee-deploymentlog -n profisee --from-file=$logfile
 
 #Make sure it installed, if not return error
-profiseeinstalledname=$(echo $(helm list --filter 'profisee+' --namespace profisee -o json)| jq '.[].name')
+profiseeinstalledname=$(echo $(helm list --filter 'profisee+' -n profisee -o json)| jq '.[].name')
 if [ -z "$profiseeinstalledname" ]; then
 	echo "Profisee did not get installed. Exiting with error";
 	exit 1
@@ -463,7 +522,7 @@ fi;
 #Wait for pod to be ready (downloaded)
 echo "Waiting for pod to be downloaded and be ready..$(date +"%Y-%m-%d %T")";
 sleep 30;
-kubectl wait --timeout=1800s --for=condition=ready pod/profisee-0 --namespace profisee
+kubectl wait --timeout=1800s --for=condition=ready pod/profisee-0 -n profisee
 
 echo $"Profisee deploymented finished $(date +"%Y-%m-%d %T")";
 
@@ -482,7 +541,7 @@ result="{\"Result\":[\
 
 echo $result
 
-kubectl delete secret profisee-deploymentlog --namespace profisee --ignore-not-found
-kubectl create secret generic profisee-deploymentlog --namespace profisee --from-file=$logfile
+kubectl delete secret profisee-deploymentlog -n profisee --ignore-not-found
+kubectl create secret generic profisee-deploymentlog -n profisee --from-file=$logfile
 
 echo $result > $AZ_SCRIPTS_OUTPUT_PATH
