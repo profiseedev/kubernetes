98b31b79c1403f563432f1e1d4e937cba41d616f
ProfiseeAdmin <91691686+ProfiseeAdmin@users.noreply.github.com>
2022-08-11 23:21:19 -0400

    Updated CSI store drive chart URL
    CSI driver has moved to https://azure.github.io/secrets-store-csi-driver-provider-azure/charts

--- Azure-ARM/deployprofisee.sh
+++ Azure-ARM/deployprofisee.sh
@@ -116,7 +116,7 @@ curl -fsSL -o Settings.yaml "$REPOURL/Azure-ARM/Settings.yaml";
 if [ "$USEKEYVAULT" = "Yes" ]; then
 	echo $"Installation of Key Vault Container Storage Interface (CSI) driver started."
 	#Install the Secrets Store CSI driver and the Azure Key Vault provider for the driver
-	helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts
+	helm repo add csi-secrets-store-provider-azure https://azure.github.io/secrets-store-csi-driver-provider-azure/charts
 	
 	#https://github.com/Azure/secrets-store-csi-driver-provider-azure/releases/tag/0.0.16
 	#The behavior changed so now you have to enable the secrets-store-csi-driver.syncSecret.enabled=true
