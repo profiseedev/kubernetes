775e8d74ac9fc8cd6f5219a51a74356cdbc76019
Yonko Ganchev <yonko.ganchev@profisee.com>
2022-09-01 21:47:57 -0400

    Update prereqcheck.sh

--- Azure-ARM/prereqcheck.sh
+++ Azure-ARM/prereqcheck.sh
@@ -139,10 +139,10 @@ if [ "$USEPURVIEW" = "Yes" ]; then
 	purviewtoken=$(curl --location --request GET "https://login.microsoftonline.com/$TENANTID/oauth2/token" --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode "client_id=$PURVIEWCLIENTID" --data-urlencode "client_secret=$PURVIEWCLIENTSECRET" --data-urlencode 'grant_type=client_credentials' --data-urlencode 'resource=https://purview.azure.net' | jq --raw-output '.access_token');
 	collectionnamenotfound=$(curl --location --request GET "$PURVIEWURL/account/collections?api-version=2019-11-01-preview" --header "Authorization: Bearer $purviewtoken" | jq --raw-output '.value | .[] | select(.friendlyName=="'$PURVIEWCOLLECTIONNAME'") | .name');
 	if [ -z "$collectionnamenotfound" ]; then
-		err=$"The $PURVIEWCOLLECTIONNAME provided could NOT be found. Exiting with error."
+		err=$"The $PURVIEWCOLLECTIONNAME collection name provided could NOT be found. Exiting with error."
 		echo $err
 	else
-		echo $" The $PURVIEWCOLLECTIONNAME provided was found. Continuing checks."
+		echo $" The $PURVIEWCOLLECTIONNAME collection name provided was found. Continuing checks."
 	fi
 fi 
 
