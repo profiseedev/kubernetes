159c067dd8bb739c88ee56285a216ff301304b0f
Naveenr04 <115646520+Naveenr04@users.noreply.github.com>
2026-01-28 13:51:45 -0500

    added kubectl version command

--- Azure-ARM/deployprofisee.sh
+++ Azure-ARM/deployprofisee.sh
@@ -109,7 +109,11 @@ echo $"Installation of kubectl started.";
 version=$(curl -sSL https://dl.k8s.io/release/stable.txt)
 echo $version
 curl -fSLO https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client --output=yaml
+
 echo $"Installation of kubectl finished.";
 
 if [ -z "$version" ]; then
